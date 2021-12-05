f5a753ceaba77f374e3d12777d900f8e2d94735c
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2021-12-05 12:33:34 +0100

    Added solution of day 4

---
+++ 4/second/solution.js
@@ -0,0 +1,71 @@
+function solution(input){
+    let lines = input.split('\n');
+    const numbers = lines[0].split(',').map(n=>+n);
+    lines = lines.splice(2);
+    const boards = [];
+    let newBoard = [];
+    for(let i = 0; i<lines.length;i++){
+        if(lines[i]=='\r'){
+            boards.push(newBoard);
+            newBoard = [];
+            continue;
+        }
+        newBoard.push(lines[i].replace(/\s+/g, ' ').trim().split(' ').map(n=>{return {value:+n,used:false};}));
+        if(i==lines.length-1){
+                        boards.push(newBoard);
+        }
+    }
+    let lastBoard;
+    for(const number of numbers){
+        let toRemove = [];
+        for(let i = 0; i<boards.length; i++){
+            const win = evaluateWin(boards[i], number);
+            if(win) toRemove.push(i);
+        }
+        if(toRemove.length>0) {
+            lastBoard = calculatePoints(boards[toRemove.length-1], number);
+        }
+        while(toRemove.length) boards.splice(toRemove.pop(),1);
+    }
+    return lastBoard;
+}
+
+function evaluateWin(board, number){
+    let rowOk = 0;
+    let colOk = 0;
+    const dim = board.length;
+    for(let i = 0; i<dim; i++){
+        rowOk = 0;
+        for(let j = 0; j<dim; j++){
+            if(board[i][j].value === number){
+                board[i][j].used = true;
+            }
+            if(board[i][j].used) rowOk++;
+        }
+        if(rowOk==dim) return true;
+    }
+    for(let i = 0; i<dim; i++){
+        colOk = 0;
+        for(let j = 0; j<dim; j++){
+            if(board[j][i].used) colOk++;
+        }
+        if(colOk==dim) return true;
+    }
+    return false;
+}
+
+function calculatePoints(board, number){
+        return countUnused(board) * number;
+}
+
+function countUnused(board){
+    const dim = board.length;
+    let points = 0;
+    for(const row of board)
+        for(const col of row)
+            if(!col.used) points+=col.value;
+    return points;
+}
+
+
+export {solution};
